f6022c10253f3888eb5fda27f9dd54fb09530999
Clem <109198546+Clementclem@users.noreply.github.com>
2025-01-17 13:30:57 +0300

    Redux allows us to maintain a global state for our applications that can be used across multiple components without prop drilling just like ContextAPI, but for bigger applications.

--- alx-project-0x06/components/layout/Header.tsx
+++ alx-project-0x06/components/layout/Header.tsx
@@ -5,10 +5,8 @@ import { RootState } from "@/store/store";
 import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
-
-  const pathname = usePathname()
-  const count = useSelector((state: RootState) => state.counter.value)
-
+  const pathname = usePathname();
+  const count = useSelector((state: RootState) => state.counter.value);
 
   return (
     <header className="fixed w-full bg-white shadow-md">
@@ -16,25 +14,15 @@ const Header: React.FC = () => {
         <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
           Splash App
         </Link>
-
-        {/* Button Group */}
         <div className="flex gap-4">
-          {
-            !["/counter-app"].includes(pathname) ? (
-              <>
-              <Button
-            buttonLabel="Sign In"
-            buttonBackgroundColor="red"
-          />
-          <Button
-            buttonLabel="Sign Up"
-            buttonBackgroundColor="blue"
-          /></>
-            ) : (
-              <p className=" font-semibold text-lg">Current count : {count}</p>
-            )
-          }
-
+          {!["/counter-app"].includes(pathname) ? (
+            <>
+              <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
+              <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
+            </>
+          ) : (
+            <p className="font-semibold text-lg">Current count: {count}</p>
+          )}
         </div>
       </div>
     </header>
